b311520a98ef830e974ea80c1ddd8cff12ee5873
toracatman <42952643+toracatman@users.noreply.github.com>
2024-07-26 13:46:23 +0900

    Update

---
+++ spgame03/game.js
@@ -0,0 +1,685 @@
+//定数宣言
+const SCREEN_W = 640;
+const SCREEN_H = 480;
+
+const MFRAMENUM = 60;
+const FRAMERATE = 60;
+
+//ゲームの状態を表す定数
+const GAME_LOAD = -1;
+const GAME_TITLE = 0;
+const GAME_MAIN = 1;
+const GAME_OVER = 2;
+const GAME_CLEAR = 3;
+
+//当たり判定モードを表す定数
+const A_GHOST = 0;	//ghost＝判定なし
+const A_NORMAL = 1;	//normal＝敵と当たれば消滅
+const A_HARD = 2;	//hard＝耐久力を持ち、当たるとlife-1される
+const A_MORTAL = 3;	//mortal＝不死
+
+//グローバル変数
+var mstarttime;
+var mcount = 0;
+
+var key_space = false;
+var key_up = false;
+var key_down = false;
+var key_left = false;
+var key_right = false;
+var key_z = false;
+var key_z_c = 0;
+
+var mouse_btn;
+var mouse_lbtn_c = 0;
+var mouse_x;
+var mouse_y;
+
+var scale;
+var draw_w;
+var draw_h;
+var draw_x;
+var draw_y;
+
+var gstate = GAME_LOAD;
+var mmode = false;
+
+var starttime;
+var animcounter;
+
+//画像読み込み
+var gametitlejpg = new Image();
+gametitlejpg.src = "media/gametitle.jpg";
+var backjpg = new Image();
+backjpg.src = "media/back01.jpg";
+
+var jikibmp = new Image();
+jikibmp.src = "media/jiki.png";
+var jitamabmp = new Image();
+jitamabmp.src = "media/tama.png";
+var tekibmp = new Image();
+tekibmp.src = "media/teki01.png";
+var bakuenbmp = new Image();
+bakuenbmp.src = "media/bakuen.png";
+var jbakuenbmp = new Image();
+jbakuenbmp.src = "media/bakuenj.png";
+var ttamabmp = new Image();
+ttamabmp.src = "media/tekitama.png";
+
+//音声読み込み
+var openmusic = new Audio("media/tam-g17.mp3");
+openmusic.loop = true;
+var backmusic = new Audio("media/Galaxy.ogg");
+backmusic.loop = true;
+var clip1 = new Audio("media/bom35.wav");
+var clip2 = new Audio("media/don18_c.wav");
+var clip3 = new Audio("media/shoot22.wav");
+var clip4 = new Audio("media/power33.wav");
+var clip5 = new Audio("media/hit_p07.wav");
+var clip7 = new Audio("media/metal27_a.wav");
+
+var jichara = {
+	img: jikibmp,
+	animpat: 3,
+	acounter: 0,
+	x: 24,
+	y: 16,
+	trigger: 0,
+	image_w: 48,
+	image_h: 32,
+	bounds_w: 32,
+	bounds_h: 20,
+	amode: A_NORMAL,
+	speed: 4,
+
+	//●自キャラデータの初期化（スタート画面から呼び出す）
+	init() {
+		this.x = 24;
+		this.y = 16;
+		this.acounter = 0;
+	},
+	move() {
+		//自キャラ移動
+		if (mmode) {
+			var dx = mouse_x - jichara.x;
+			var dy = mouse_y - jichara.y;
+			var d = Math.atan2(dy, dx);
+			var sx = Math.cos(d);
+			var sy = Math.sin(d);
+			var s = Math.min(this.speed, Math.abs(dx + dy));
+			this.x += sx * s;
+			this.y += sy * s;
+		}
+		else {
+			if (key_up)		this.y -= this.speed;
+			if (key_down)	this.y += this.speed;
+			if (key_left)	this.x -= this.speed;
+			if (key_right)	this.x += this.speed;
+		}
+		if (this.x < 24)	this.x = 24;
+		if (this.y < 16)	this.y = 16;
+		if (this.x > 616)	this.x = 616;
+		if (this.y > 464)	this.y = 464;
+		
+		//弾の発射
+		if ((key_z || mmode) && this.trigger == 0) {
+			if (jitamas.length < 3) {
+				var jt = {
+					img: jitamabmp,
+					x: this.x,
+					y: this.y,
+					life: 1,
+					image_w: 64,
+					image_h: 16,
+					bounds_w: 48,
+					bounds_h: 12,
+					amode: A_NORMAL,
+					speed: 10,
+					move() {
+						//弾の移動
+						this.x += this.speed;
+
+						//画面外に出たときの処理
+						if (this.x > 672) this.life = 0;
+					}
+				};
+				jitamas.push(jt);
+			}
+			this.trigger = 20;
+		}
+		if (this.trigger > 0) this.trigger--;
+	}
+};
+var jitamas = [];
+var tekis = [];
+var bakuens = [];
+var hantens = [];
+var jbakuen = {
+	img: jbakuenbmp,
+	animpat: 4,
+	acounter: 0,
+	x: 0,
+	y: 0,
+	image_w: 48,
+	image_h: 48
+};
+var boss = {
+	x: 200,
+	y: 200,
+	r: 7,
+	c: 6,
+	m: [ [60, 0, 2], [120, 0, -2], [60, 0, 2], [100, -4, 0], [100, 4, 0] ],
+	cp: 0,
+	rp: 0,
+
+	//●敵キャラデータの初期化（スタート画面から呼び出す）
+	init() {
+		this.x = 400;
+		this.y = 200;
+		this.d = 45;
+		var a = [
+			["B", "N", "H", "N", "N", "H", "N"],
+			["H", "N", "N", "N", "N", "N", "N"],
+			["S", "S", "S", "N", "N", "N", "N"],
+			["B", "S", "S", "S", "S", "C", "N"],
+			["S", "S", "S", "N", "N", "N", "N"],
+			["H", "N", "H", "N", "N", "H", "N"]
+		];
+		tekis = [];
+		for (var y = 0; y < this.c; y++) {
+			for (var x = 0; x < this.r; x++) {
+				var t = {
+					img: tekibmp,
+					sx: x,
+					sy: y,
+					animpat: 1,
+					acounter: 0,
+					life: 1,
+					image_w: 32,
+					image_h: 32,
+					bounds_w: 32,
+					bounds_h: 32,
+					x: boss.x - (this.image_w * boss.r >> 1) + this.sx * this.image_w + (this.image_w >> 1),
+					y: boss.y - (this.image_h * boss.c >> 1) + this.sy * this.image_h + (this.image_w >> 1),
+					amode: A_MORTAL,
+					move() {
+						this.x = boss.x - (this.image_w * boss.r >> 1) + this.sx * this.image_w + (this.image_w >> 1);
+						this.y = boss.y - (this.image_h * boss.c >> 1) + this.sy * this.image_h + (this.image_w >> 1);
+						if (this.h && (Math.random() * 99 | 0) == 1 && boss.m[boss.cp][1] == 0) {
+							var d = Math.atan2(jichara.y - this.y, jichara.x - this.x);
+							var tt = {
+								img: ttamabmp,
+								x: this.x,
+								y: this.y,
+								life: 1,
+								image_w: 12,
+								image_h: 12,
+								bounds_w: 8,
+								bounds_h: 8,
+								amode: A_NORMAL,
+								sx: Math.cos(d),
+								sy: Math.sin(d),
+								s: 4,
+								move() {
+									//弾の移動
+									this.x += this.sx * this.s;
+									this.y += this.sy * this.s;
+
+									//画面外に出たときの処理
+									if (this.x < -6 || this.y < -6 || this.x > 646 || this.y > 486) this.life = 0;
+								}
+							};
+							ttamas.push(tt);
+							stopAndPlay(clip3);
+						}
+					}
+				};
+				switch (a[y][x]) {
+				case "B":
+					t.amode = A_GHOST;
+					t.life = 1;
+					break;
+				case "C":
+					t.amode = A_HARD;
+					t.life = 2;
+					t.core = true;
+					break;
+				case "N":
+					t.amode = A_MORTAL;
+					t.life = 1;
+					break;
+				case "S":
+					t.amode = A_HARD;
+					t.life = 4;
+					break;
+				case "H":
+					t.amode = A_HARD;
+					t.life = 12;
+					t.h = true;
+					break;
+				}
+				tekis.push(t);
+			}
+		}
+		this.cp = 0;
+		this.rp = this.m[this.cp][0];
+	},
+	move() {
+		this.d = (this.d + this.m[this.cp][1] + this.m[this.cp][2] + 360) % 360;
+		this.x += -Math.cos(this.d * Math.PI / 180);
+		this.y += Math.sin(this.d * Math.PI / 180);
+		this.rp--;
+		if (this.rp == 0) {
+			this.cp++;
+			if (this.cp == this.m.length) this.cp = 0;
+			this.rp = this.m[this.cp][0];
+		}
+	},
+	vanish() {
+		this.y += 4;
+		this.x += ((animcounter / 4 | 0) % 2 * 8 - 4);
+		if (animcounter % 8 == 0) setBakuen(boss.x - (this.r * 32 >> 1) + 16 + (Math.random() * this.r | 0) * 32, boss.y - (this.c * 32 >> 1) + 16 + (Math.random() * this.c | 0) * 32);
+	}
+};
+ttamas = [];
+
+//キャンバス、コンテキストの取得
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+//キーボード
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Up" || e.key === "ArrowUp")			key_up = true;
+	if (e.key === "Down" || e.key === "ArrowDown")		key_down = true;
+	if (e.key === "Left" || e.key === "ArrowLeft")		key_left = true;
+	if (e.key === "Right" || e.key === "ArrowRight")	key_right = true;
+	if (e.key === "z" || e.key == "Z")					key_z = true;
+});
+document.addEventListener("keyup", (e) => {
+	if (e.key === "Up" || e.key === "ArrowUp")			key_up = false;
+	if (e.key === "Down" || e.key === "ArrowDown")		key_down = false;
+	if (e.key === "Left" || e.key === "ArrowLeft")		key_left = false;
+	if (e.key === "Right" || e.key === "ArrowRight")	key_right = false;
+	if (e.key === "z" || e.key == "Z")					key_z = false;
+});
+
+//マウス
+document.addEventListener("mousedown", (e) => {
+	mouse_btn = e.buttons;
+});
+document.addEventListener("mouseup", (e) => {
+	mouse_btn = e.buttons;
+});
+document.addEventListener("mousemove", (e) => {
+	mouse_x = (e.clientX - draw_x) / scale;
+	mouse_y = (e.clientY - draw_y) / scale;
+});
+
+//●メインループ
+function main() {
+	updateFrameRate();
+
+	if (key_z)	key_z_c++;
+	else		key_z_c = 0;
+	if (mouse_btn & 1)	mouse_lbtn_c++;
+	else				mouse_lbtn_c = 0;
+
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	scale = Math.min(canvas.width / SCREEN_W, canvas.height / SCREEN_H);
+	draw_w = SCREEN_W * scale;
+	draw_h = SCREEN_H * scale;
+	draw_x = (canvas.width - draw_w) >> 1;
+	draw_y = (canvas.height - draw_h) >> 1;
+	ctx.translate(draw_x, draw_y);
+	ctx.scale(scale, scale);
+
+	switch (gstate) {
+	case GAME_LOAD:
+		gameLoad();
+		break;
+	case GAME_TITLE:
+		gameTitle();
+		break;
+	case GAME_MAIN:
+		gameMain();
+		break;
+	case GAME_OVER:
+		gameOver();
+		break;
+	case GAME_CLEAR:
+		gameClear();
+		break;
+	}
+
+	ctx.resetTransform();
+	ctx.clearRect(0, 0, draw_x, canvas.height);
+	ctx.clearRect(draw_x + draw_w, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, draw_y);
+	ctx.clearRect(0, draw_y + draw_h, canvas.width, canvas.height);
+
+	ctx.fillStyle = "#fff";
+	ctx.font = "16px 'Mkana+'";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText(`${mouse_x} ${mouse_y}`, 50, 50);
+
+	animcounter++;
+	waitFrameRate();
+}
+main();
+
+//●ゲーム読み込み画面
+function gameLoad() {
+	if (key_z || (mouse_btn & 1)) {
+		gstate = GAME_TITLE;
+		if (mouse_btn & 1) mmode = true;
+		openmusic.play();
+	}
+
+	ctx.fillStyle = "#fff";
+	ctx.font = "32px 'Mkana+'";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillText("Zキーでキーボードモードでゲーム開始", SCREEN_W >> 1, (SCREEN_H >> 1) - 32);
+	ctx.fillText("クリックでマウスモードでゲーム開始", SCREEN_W >> 1, SCREEN_H >> 1);
+	ctx.fillText("F11キーで全画面", SCREEN_W >> 1, (SCREEN_H >> 1) + 32);
+}
+
+//●ゲームタイトル画面
+function gameTitle() {
+	if (key_z_c == 1 || mouse_lbtn_c == 1) {
+		gstate = GAME_MAIN;
+		jichara.init();
+		jitamas = [];
+		boss.init();
+		ttamas = [];
+		bakuens = [];
+		hantens = [];
+		animcounter = 0;
+		stop(openmusic);
+		stopAndPlay(clip4);
+		backmusic.play();
+	}
+
+	ctx.drawImage(gametitlejpg, 0, 60);
+	ctx.fillStyle = "#f0f";
+	ctx.font = "bold 32px 'Mkana+'";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillText(mmode ? "CLICK TO START" : "PRESS Z KEY", SCREEN_W >> 1, 300);
+}
+
+//●ゲーム本体の実行
+function gameMain() {
+	jichara.move();
+	for (var i = 0; i < jitamas.length; i++) jitamas[i].move();
+	boss.move();
+	for (var i = 0; i < tekis.length; i++) tekis[i].move();
+	for (var i = 0; i < ttamas.length; i++) ttamas[i].move();
+	var a = atariHantei();
+	if (a == 1) {
+		gstate = GAME_OVER;
+		starttime = Date.now();
+		animcounter = 0;
+		jbakuen.acounter = 16;
+		stop(backmusic);
+		stopAndPlay(clip1);
+	}
+	else if (a == 2) {
+		gstate = GAME_CLEAR;
+		starttime = Date.now();
+		animcounter = 0;
+		stop(backmusic);
+	}
+
+	drawGame(true);
+}
+
+//●ゲームオーバー画面
+function gameOver() {
+	if (Date.now() - starttime >= 5000) {
+		gstate = GAME_TITLE;
+		openmusic.play();
+	}
+
+	drawGame(false);
+
+	//爆炎表示
+	if (jbakuen.acounter == 16) {
+		jbakuen.acounter = 0;
+		jbakuen.x = jichara.x + (Math.random() * 64 | 0) - 32;
+		jbakuen.y = jichara.y + (Math.random() * 64 | 0) - 32;
+	}
+	var sx = (jbakuen.acounter / 4 | 0) % jbakuen.animpat * jbakuen.image_w;
+	ctx.drawImage(jbakuen.img, sx, 0, jbakuen.image_w, jbakuen.image_h, jbakuen.x - (jbakuen.image_w >> 1), jbakuen.y - (jbakuen.image_h >> 1), jbakuen.image_w, jbakuen.image_h);
+	jbakuen.acounter++;
+
+	ctx.font = "bold 100px 'Mkana+'";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillStyle = "#000";
+	ctx.fillText("GAME OVER",2 + SCREEN_W >> 1, 2 + SCREEN_H >> 1);
+	ctx.fillStyle = "#f00";
+	ctx.fillText("GAME OVER", SCREEN_W >> 1, SCREEN_H >> 1);
+}
+
+//●ゲームクリアー画面
+function gameClear() {
+	boss.vanish();
+	for (var i = 0; i < tekis.length; i++) tekis[i].move();
+	if (Date.now() - starttime >= 5000) {
+		gstate = GAME_TITLE;
+		openmusic.play();
+	}
+
+	drawGame(false);
+
+	ctx.font = "bold 100px 'Mkana+'";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillStyle = "#000";
+	ctx.fillText("GAME CLEAR",2 + SCREEN_W >> 1, 2 + SCREEN_H >> 1);
+	ctx.fillStyle = "#0ff";
+	ctx.fillText("GAME CLEAR", SCREEN_W >> 1, SCREEN_H >> 1);
+}
+
+//●ゲーム画面の表示
+function drawGame(tama) {
+	ctx.drawImage(backjpg, 0, 0);
+
+	var sx;
+	sx = (jichara.acounter / 6 | 0) % jichara.animpat * jichara.image_w;
+	ctx.drawImage(jichara.img, sx, 0, jichara.image_w, jichara.image_h, jichara.x - (jichara.image_w >> 1), jichara.y - (jichara.image_h >> 1), jichara.image_w, jichara.image_h);
+	jichara.acounter++;
+
+	if (tama) {
+		for (var i = 0; i < jitamas.length; i++) {
+			var jt = jitamas[i];
+			if (jt.life > 0) ctx.drawImage(jt.img, jt.x - (jt.image_w >> 1), jt.y - (jt.image_h >> 1));
+			else {
+				jitamas.splice(i, 1);
+				i--;
+			}
+		}
+	}
+
+	for (var i = 0; i < tekis.length; i++) {
+		var t = tekis[i];
+		if (t.life > 0) {
+			sx = t.sx * 32 + (t.acounter / 20 | 0) % t.animpat * t.image_w;
+			var sy = t.sy * 32;
+			ctx.drawImage(t.img, sx, sy, t.image_w, t.image_h, t.x - (t.image_w >> 1), t.y - (t.image_h >> 1), t.image_w, t.image_h);
+			t.acounter++;
+		}
+		else {
+			tekis.splice(i, 1);
+			i--;
+		}
+	}
+
+	if (tama) {
+		for (var i = 0; i < ttamas.length; i++) {
+			var tt = ttamas[i];
+			if (tt.life > 0) {
+				sx = 0;
+				ctx.drawImage(tt.img, sx, 0, tt.image_w, tt.image_h, tt.x - (tt.image_w >> 1), tt.y - (tt.image_h >> 1), tt.image_w, tt.image_h);
+			}
+			else {
+				ttamas.splice(i, 1);
+				i--;
+			}
+		}
+	}
+
+	for (var i = 0; i < bakuens.length; i++) {
+		var b = bakuens[i];
+		sx = (b.acounter / 4 | 0) % b.animpat * b.image_w;
+		ctx.drawImage(b.img, sx, 0, b.image_w, b.image_h, b.x - (b.image_w >> 1), b.y - (b.image_h >> 1), b.image_w, b.image_h);
+		b.acounter++;
+		if (bakuens[i].acounter == 16) {
+			bakuens.splice(i, 1);
+			i--;
+		}
+	}
+
+	for (var i = 0; i < hantens.length; i++) {
+		var h = hantens[i];
+		ctx.fillStyle = "rgba(255, 0, 255, 0.5)";
+		ctx.fillRect(h.x - (h.image_w >> 1), h.y - (h.image_h >> 1), h.image_w, h.image_h);
+		h.acounter++;
+		if (hantens[i].acounter == 4) {
+			hantens.splice(i, 1);
+			i--;
+		}
+	}
+
+	/*ctx.fillStyle = "#f00";
+	ctx.beginPath();
+	ctx.arc(boss.x, boss.y, 10, 0, Math.PI * 2);
+	ctx.fill();*/
+}
+
+function updateFrameRate() {
+	if (mcount == 0) mstarttime = Date.now();	//1フレーム目なら時刻を計測
+	if (mcount == MFRAMENUM) {					//測定するフレーム目なら繰り返す
+		mstarttime = Date.now();
+		mcount = 0;
+	}
+	mcount++;
+}
+
+function waitFrameRate() {
+	var tooktime = Date.now() - mstarttime;						//かかった時間
+	var waittime = (mcount * 1000 / FRAMERATE | 0) - tooktime;	//待つべき時間
+	setTimeout(main, waittime);									//待機
+}
+
+//●停止
+function stop(s) {
+	s.pause();
+	s.currentTime = 0;
+}
+
+//●停止して再生
+function stopAndPlay(s) {
+	s.pause();
+	s.currentTime = 0;
+	s.play();
+}
+
+function isAtari(a, b) {
+	var retval = false;
+	var ax1 = a.x - (a.bounds_w >> 1);
+	var ay1 = a.y - (a.bounds_h >> 1);
+	var ax2 = a.x + (a.bounds_w >> 1);
+	var ay2 = a.y + (a.bounds_h >> 1);
+	var bx1 = b.x - (b.bounds_w >> 1);
+	var by1 = b.y - (b.bounds_h >> 1);
+	var bx2 = b.x + (b.bounds_w >> 1);
+	var by2 = b.y + (b.bounds_h >> 1);
+	if (ax1 < bx2 && bx1 < ax2 && ay1 < by2 && by1 < ay2) {
+		switch (a.amode) {
+		case A_NORMAL:	//キャラ消滅
+			a.life = 0;
+			break;
+		case A_HARD:	//耐久力-1
+			a.life--;
+			if (a.life < 0) a.life = 0;
+			break;
+		case A_MORTAL:	//音を鳴らす
+			stopAndPlay(clip7);
+			break;
+		}
+		switch (b.amode) {
+		case A_NORMAL:	//キャラ消滅
+			b.life = 0;
+			break;
+		case A_HARD:	//耐久力-1
+			b.life--;
+			if (b.life < 0) b.life = 0;
+			if (b.life > 0) setHanten(b.x, b.y, b.image_w, b.image_h);
+			break;
+		case A_MORTAL:	//音を鳴らす
+			stopAndPlay(clip7);
+			break;
+		}
+		if (b.life == 0) setBakuen(b.x, b.y);
+		retval = true;
+	}
+	return retval;
+}
+
+function atariHantei() {
+	for (var i = 0; i < tekis.length; i++) {
+		var t = tekis[i];
+		if (t.life > 0 && t.amode != A_GHOST) {
+			//自機と敵の判定
+			if (isAtari(jichara, t)) return 1;
+			//弾と敵の判定
+			for (var j = 0; j < jitamas.length; j++) {
+				var jt = jitamas[j];
+				if (jt.life > 0) isAtari(jt, t);
+			}
+			//ゲームクリア
+			if (t.core && t.life == 0) return 2;
+		}
+	}
+	for (var i = 0; i < ttamas.length; i++) {
+		var tt = ttamas[i];
+		if (tt.life > 0 && tt.amode != A_GHOST) {
+			//自機と敵弾の判定
+			if (isAtari(jichara, tt)) return 1;
+		}
+	}
+	return 0;
+}
+
+//●爆炎セット
+function setBakuen(x, y) {
+	var b = {
+		img: bakuenbmp,
+		animpat: 4,
+		acounter: 0,
+		x: x,
+		y: y,
+		image_w: 32,
+		image_h: 32
+	}
+	bakuens.push(b);
+	stopAndPlay(clip2);
+}
+
+//●A_HARD用の反転セット
+function setHanten(x, y, w, h) {
+	var h = {
+		acounter: 0,
+		x: x,
+		y: y,
+		image_w: w,
+		image_h: h
+	}
+	hantens.push(h);
+	stopAndPlay(clip5);
+}
---
+++ spgame03/random.js
@@ -0,0 +1,23 @@
+class Random {
+  constructor(seed = 88675123) {
+    this.x = 123456789;
+    this.y = 362436069;
+    this.z = 521288629;
+    this.w = seed;
+  }
+  
+  // XorShift
+  next() {
+    let t;
+ 
+    t = this.x ^ (this.x << 11);
+    this.x = this.y; this.y = this.z; this.z = this.w;
+    return this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8)); 
+  }
+  
+  // min以上max以下の乱数を生成する
+  nextInt(min, max) {
+    const r = Math.abs(this.next());
+    return min + (r % (max + 1 - min));
+  }
+}
